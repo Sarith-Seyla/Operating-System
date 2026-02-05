435d2c21f01fed81de26bcb9249b9fe61d894123
Sarith-Seyla <sarithseyla9999@gmail.com>
2026-02-05 12:01:23 -0500

    class activity 1

---
+++ copyfilesyscall.c
@@ -0,0 +1,48 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+int main() {
+    int source_fd, dest_fd;
+    ssize_t bytes_read, bytes_written;
+    char buffer[BUFFER_SIZE];
+
+    // Open source file
+    source_fd = open("result.txt", O_RDONLY);
+    if (source_fd < 0) {
+        perror("Error opening source file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open destination file
+    dest_fd = open("copyresult.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (dest_fd < 0) {
+        perror("Error opening destination file");
+        close(source_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Copy contents
+    while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0) {
+        bytes_written = write(dest_fd, buffer, bytes_read);
+        if (bytes_written != bytes_read) {
+            perror("Error writing to destination file");
+            close(source_fd);
+            close(dest_fd);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (bytes_read < 0) {
+        perror("Error reading source file");
+    }
+
+    // Close files
+    close(source_fd);
+    close(dest_fd);
+
+    return 0;
+}
